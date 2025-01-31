4ad880a0bb9d454ed44e19f51c9bcb66eb55e1e1
lenaKacher <156103829+lenaKacher@users.noreply.github.com>
2025-01-31 17:20:28 +0100

    Update SeleniumBuyProductsTest.java

--- selenium/java/src/test/java/de/proficom/guitests/SeleniumBuyProductsTest.java
+++ selenium/java/src/test/java/de/proficom/guitests/SeleniumBuyProductsTest.java
@@ -48,11 +48,13 @@ public class SeleniumBuyProductsTest {
 
 
     @ClassRule
-    public static BrowserWebDriverContainer chrome =
+    public static BrowserWebDriverContainer chrome   
         new BrowserWebDriverContainer<>(DockerImageName.parse("selenium/standalone-chrome:126.0"))
                     //.withRecordingMode(VncRecordingMode.RECORD_ALL, file, VncRecordingFormat.MP4)
                     //.withRecordingFileFactory(new DefaultRecordingFileFactory())
-                    .withCapabilities(new ChromeOptions());
+                     .withNetworkMode("host")  
+                     .withCreateContainerCmdModifier(cmd -> cmd.getHostConfig().withShmSize(512 * 1024 * 1024));
+                     .withCapabilities(new ChromeOptions());
             
 
     @BeforeClass
